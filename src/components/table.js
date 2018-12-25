794ea9e4467b73ea4f118abef2d75e6bcffa92ab
yacine <yacinebenz3@gmail.com>
2018-12-25 19:31:42 +0000

    added more data

--- src/components/table.js
+++ src/components/table.js
@@ -3,43 +3,75 @@ import axios from 'axios';
 import './table.css';
 let NumberFormat = require('react-number-format');
 
+
+
+
+const CRYPTOCOMPARE_API_URI = 'https://www.cryptocompare.com/api/data/coinlist/';
+const COINMARKETCAP_API_URI = 'https://api.coinmarketcap.com/v1/ticker/?limit=20'; 
+const CRYPTOLEXT = 'https://www.cryptocompare.com/';
+
+
 class Table extends Component {
 
 
      constructor(props) {
          super(props);
          this.state = {
-             cryptos: []
+                coinsData: [],
+                coinsMetaData: {},
+                cryptoCompareOk: false,
          };
+        this.loadData = this.loadData.bind(this);
      }
-     componentDidMount() {
-         axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,XRP,LTC,EOS,ADA,XLM,ETH,IOT&tsyms=USD')
-             .then(res => {
-                 const cryptos = res.data;
-                 this.setState({
-                     cryptos: cryptos
-                 })
-             })
-     }
+
+      async loadData() {
+          const coinsData = await axios.get(COINMARKETCAP_API_URI);
+          this.setState({
+              coinsData: coinsData.data,
+          });
+
+          const coinsMetaData = await axios.get(CRYPTOCOMPARE_API_URI);
+          this.setState({
+              coinsMetaData: coinsMetaData.data.Data,
+              cryptoCompareOk: true,
+          });
+      }
+
+      async componentDidMount() {
+          await this.loadData();
+          setInterval(this.loadData, 4000);
+      }
+
+      getCoinImage(symbol) {
+          if (!this.state.cryptoCompareOk) return "";
+          return CRYPTOLEXT + this.state.coinsMetaData[symbol];
+      }
+
     render() {
-        return (
+
+                 return (
             <div class="card main-table">
-    <table class="table">
-    <div id="container">
-    <tbody>
-        {
-            Object.keys(this.state.cryptos).map((key) => (
-                    <tr>
-                      <th scope="row">{key}</th>
-                      <td>{this.state.cryptos[key].symbol}</td>
-                      <td><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'}/></td>
-                    </tr>      
-          ))}
-          </tbody>
-          </div>
-          </table>
+                    <table class="table">
+                    <div id="container">
+                            <tbody>
+                                {
+                                    this.state.coinsData.map((coin, index) => (
+                                            <tr>
+                                            <th scope="row">{coin.name}</th>
+                                            <td>{coin.symbol}</td>
+
+                                            <td><NumberFormat value={coin.price_usd} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'}/></td>
+                                            <td>{coin.percent_change_1h}</td>
+                                            <td>{coin.percent_change_24h}</td>
+                                            <td>{coin.percent_change_7d}</td>
+                                            </tr>      
+                                ))}
+                                </tbody>
+                        </div>
+                        </table>
             </div>
         );
+
     }
 }
 
